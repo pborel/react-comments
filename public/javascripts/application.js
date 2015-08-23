ab8ec28054ad6bcc946c9c5622cb57bdc19f9ee0
Phil Borel <email": null>
2015-08-22 21:50:59 -0700

    update comment load for optimistic loading

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -41,6 +41,10 @@ var CommentBox = React.createClass({
   },
 
   handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+
     $.ajax({
       url: this.props.url,
       dataType: 'json',
